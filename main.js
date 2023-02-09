a140f3cff48aaab07ca4adfbc1c92e555fe6bd9e
StevenEduardoReyesLeon <stevenrl325@gmail.com>
2023-02-09 09:42:00 -0500

    Operadores Aritmeticos

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let num1 = 50; let num2 = 30;
+let resultado;
+
+resultado = num1 + num2
+console.log(`${num1} + ${num2} = ${resultado}`);
+
+resultado = num1 - num2;
+console.log(`${num1} - ${num2} = ${resultado}`);
+
+resultado = num1 / num2;
+console.log(`${num1} / ${num2} = ${resultado}`);
+
+resultado = num1 ** num2;
+console.log(`${num1} ** ${num2} = ${resultado}`);
+
+resultado = num1 * num2;
+console.log(`${num1} * ${num2} = ${resultado}`);
+
+resultado = num1 % num2;
+console.log(`${num1} % ${num2} = ${resultado}`);
+
+resultado = -num1;
+console.log(`${num1} Negativo ${resultado}`);
+
